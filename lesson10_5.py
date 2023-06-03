764c7ad7f7357dba64516be7ed33739cc5834c07
Cuei-Sian <doreen20080615@gmail.com>
2023-06-03 14:46:26 +0800

    modify

---
+++ lesson10_5.py
@@ -0,0 +1,11 @@
+#多選項
+import streamlit as st
+
+import streamlit as st
+
+options = st.multiselect(
+    '請選擇您喜歡的顏色',
+    ['Green', 'Yellow', 'Red', 'Blue'],
+    ['Yellow', 'Red'])
+
+st.write('您選擇:', options)
