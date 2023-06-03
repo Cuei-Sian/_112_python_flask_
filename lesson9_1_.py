507f373c3de1991cff982f5d0766ea615a55c1d1
Cuei-Sian <doreen20080615@gmail.com>
2023-06-03 10:56:30 +0800

    modity

---
+++ lesson9_1_.py
@@ -0,0 +1,17 @@
+#在網頁顯示文字
+import streamlit as st
+
+"### Hello world Doreen"
+"Hello ***王翠賢***"
+
+st.markdown("Hello! **world**!")
+st.divider()
+st.title("這是app的title")
+st.divider()
+st.header("這是header")
+st.divider()
+st.subheader("這是subheader")
+st.divider()
+st.caption("這是caption")
+st.divider()
+st.code("a=123")
