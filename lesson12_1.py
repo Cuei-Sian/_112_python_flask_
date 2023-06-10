6fc27992d2cebbac36bd86ce91af724ca5f039ea
Cuei-Sian <132725542+Cuei-Sian@users.noreply.github.com>
2023-06-10 06:56:22 +0000

    modify

--- lesson12_1.py
+++ lesson12_1.py
@@ -1,5 +1,8 @@
 import streamlit as st
+import pandas as pd
+
+pd.read_csv('codeSearch.csv',usecols=['code','name'])
+codeSeries = codeFrame['code'].astype(str) + codeFrame['name']
 
 with st.sidebar:
-    st.write("請選擇股票號碼:")
-    
+    st.write("請選擇股票號碼:",codeSeries)
